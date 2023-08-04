2d862af294966e7ca7cf19459156ab52d1d5afda
Thalia <thaliatelopoulou@gmail.com>
2023-08-04 19:36:55 +0300

    exercise 4 done

--- practice/server.mjs
+++ practice/server.mjs
@@ -8,10 +8,11 @@ const server = createServer((request, response) => {
   response.setHeader("Content-Type", "text/html");
 
   response.end(
-    "<html><body><h1>Hello user!</h1></body></html>"
+    "<html><body><h1>This page was served with Node.js</h1></body></html>"
   );
 });
 
-server.listen(3000, () => {
-  console.log(`Server running at http://localhost:3000`);
+
+server.listen(3000, ()=> {
+    console.log(`Server is running at http://localhost:3000`)
 });
