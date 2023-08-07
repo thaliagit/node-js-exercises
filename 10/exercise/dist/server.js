53144d8317ab4af68d3721d9d87d6c8dcd21868a
Thalia <thaliatelopoulou@gmail.com>
2023-08-07 21:31:55 +0300

    exercise 10 completed

---
+++ 10/exercise/dist/server.js
@@ -0,0 +1,31 @@
+// const TODO: string = "start writing your Express API server here :)";
+// console.log(TODO);
+import express from "express";
+import dotenv from "dotenv";
+import 'express-async-errors';
+import morgan from "morgan";
+dotenv.config();
+let planets = [
+    {
+        id: 1,
+        name: 'Earth',
+    },
+    {
+        id: 2,
+        name: 'Mars',
+    },
+];
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.get('/planets', (req, res) => {
+    res.json(planets);
+});
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
